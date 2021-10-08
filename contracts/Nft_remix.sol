db9ed8a0c3a37d4d3f3a40f8140671a666b417ee
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2021-10-08 22:52:23 +0800

    tokenURI is WIP

--- contracts/Nft_remix.sol
+++ contracts/Nft_remix.sol
@@ -9,11 +9,109 @@ import 'https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/ma
 
 contract NFT is ERC721, Ownable, ERC721Enumerable, ERC721Pausable {
     using Counters for Counters.Counter;
+    using Strings for uint;
+    
     Counters.Counter public nextId;
     
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
-        _safeMint(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, nextId.current());
-        nextId.increment();
+    uint256 public cost = 10 ether;
+    uint256 public maxSupply = 10000;
+    uint256 public maxMintAmount = 2;
+    
+    string public baseURI;
+    string public baseExtension = ".json";
+    string public notRevealedUri;
+    
+    bool public pauseMintingState = false;
+    bool public revealed = false;
+    
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        string memory _initBaseURI,
+        string memory _initNotRevealedUri
+    ) ERC721(_name, _symbol) {
+        mint(msg.sender, 2);
+        baseURI = _initBaseURI;
+        notRevealedUri = _initNotRevealedUri;
+    }
+    
+    // public
+    function mint(address _to, uint _mintAmount) public payable {
+        require(!pauseMintingState, 'Minting is paused.');
+        require(_mintAmount > 0, 'Mint amount must be greater than 0.');
+        require(_mintAmount <= maxMintAmount, 'Mint amount must not be greater than maxMintAmount');
+        require(nextId.current() + _mintAmount <= maxSupply, 'Cannot mint more than total supply.');
+
+        if (msg.sender != owner()) {
+            require(msg.value >= cost * _mintAmount);
+        }
+
+        for (uint i = 1; i <= _mintAmount; i++) {
+            nextId.increment();
+            _safeMint(_to, nextId.current());
+        }
+    }
+    
+    function walletOfOwner(address _owner) 
+    public
+    view
+    returns (uint[] memory) {
+        uint ownerTokenCount = balanceOf(_owner);
+        uint[] memory tokenIds = new uint[](ownerTokenCount);
+        for (uint i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+    
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory) {
+        require(
+          _exists(tokenId),
+          "ERC721Metadata: URI query for nonexistent token"
+        );
+        
+        if(revealed == false) {
+            return notRevealedUri;
+        }
+        
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0
+            ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+            : "";
+    }
+    
+    //only owner
+    function reveal() public onlyOwner() {
+      revealed = true;
+    }
+    
+    function setCost(uint256 _newCost) public onlyOwner() {
+        cost = _newCost;
+    }
+    
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
+        maxMintAmount = _newmaxMintAmount;
+    }
+    
+    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+        notRevealedUri = _notRevealedURI;
+    }
+    
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+    
+    function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+        baseExtension = _newBaseExtension;
+    }
+    
+    function setPauseMinting(bool _state) public onlyOwner {
+        pauseMintingState = _state;
     }
     
     // Pause or unpause transfers
@@ -24,6 +122,17 @@ contract NFT is ERC721, Ownable, ERC721Enumerable, ERC721Pausable {
         } else {
             _unpause();
         }
+    }
+    
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success);
+    }
+    
+    
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
     }
     
     // override inherited contracts
