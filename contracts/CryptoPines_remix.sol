1ccc283efce6bbd959d675322c3957f5af7e7e2e
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2021-10-10 21:33:07 +0800

    Added randomize minting

--- contracts/CryptoPines_remix.sol
+++ contracts/CryptoPines_remix.sol
@@ -10,7 +10,7 @@ contract NFT is ERC721, Ownable, ERC721Enumerable {
     using Counters for Counters.Counter;
     using Strings for uint;
     
-    Counters.Counter public nextId;
+    Counters.Counter public tokenCount;
     
     uint256 public cost = 2000000 gwei;
     uint256 public maxSupply = 10000;
@@ -23,36 +23,37 @@ contract NFT is ERC721, Ownable, ERC721Enumerable {
     bool public pauseMintingState = false;
     bool public revealed = false;
     
+    // Used for random index assignment
+    mapping(uint => uint) public tokenMatrix;
+
+    // The initial token ID
+    uint public startFrom = 1;
+    
     constructor() ERC721("CryptoPines Test", "PINES") {
         mint(6);
     }
     
     // public
-    function mint(uint _mintAmount) public payable {
+    function mint() external payable {
+        require(!pauseMintingState, 'Minting is paused.');
+        require(_tokenCount.current() + 1 <= maxSupply, 'Cannot mint more than total supply.');
+        if (msg.sender != owner()) {
+            require(msg.value >= cost);
+        }
+        _safeMint(msg.sender, nextToken());
+    }
+    
+    function mintTo(address _to, uint _mintAmount) external ensureAvailabilityFor(_mintAmount) payable {
         require(!pauseMintingState, 'Minting is paused.');
         require(_mintAmount > 0, 'Mint amount must be greater than 0.');
         require(_mintAmount <= maxMintAmount, 'Mint amount must not be greater than maxMintAmount');
-        require(nextId.current() + _mintAmount <= maxSupply, 'Cannot mint more than total supply.');
-
+        
         if (msg.sender != owner()) {
             require(msg.value >= cost * _mintAmount);
         }
-
+        
         for (uint i = 1; i <= _mintAmount; i++) {
-            nextId.increment();
-            _safeMint(msg.sender, nextId.current());
-        }
-    }
-    
-    function mintTo(address _to, uint _mintAmount) public payable onlyOwner {
-        require(!pauseMintingState, 'Minting is paused.');
-        require(_mintAmount > 0, 'Mint amount must be greater than 0.');
-        require(_mintAmount <= maxMintAmount, 'Mint amount must not be greater than maxMintAmount');
-        require(nextId.current() + _mintAmount <= maxSupply, 'Cannot mint more than total supply.');
-
-        for (uint i = 1; i <= _mintAmount; i++) {
-            nextId.increment();
-            _safeMint(_to, nextId.current());
+            _safeMint(_to, nextToken());
         }
     }
     
@@ -123,6 +124,12 @@ contract NFT is ERC721, Ownable, ERC721Enumerable {
         require(success);
     }
     
+    /// @dev Check whether tokens are still available
+    /// @return the available token count
+    function availableTokenCount() public view returns (uint) {
+        return maxSupply - _tokenCount.current();
+    }
+    
     
     // internal
     function _baseURI() internal view virtual override returns (string memory) {
@@ -136,5 +143,68 @@ contract NFT is ERC721, Ownable, ERC721Enumerable {
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+    
+    /// Get the next token ID
+    /// @dev Randomly gets a new token ID and keeps track of the ones that are still available.
+    /// @return the next token ID
+    function nextToken() internal ensureAvailability returns (uint) {
+        uint maxIndex = availableTokenCount();
+        uint random = uint(keccak256(
+            abi.encodePacked(
+                msg.sender,
+                block.coinbase,
+                block.difficulty,
+                block.gaslimit,
+                block.timestamp
+            )
+        )) % maxIndex;
+
+        uint value = 0;
+        if (tokenMatrix[random] == 0) {
+            // If this matrix position is empty, set the value to the generated random number.
+            value = random;
+        } else {
+            // Otherwise, use the previously stored number from the matrix.
+            value = tokenMatrix[random];
+        }
+
+        // If the last available tokenID is still unused...
+        if (tokenMatrix[maxIndex - 1] == 0) {
+            // ...store that ID in the current matrix position.
+            tokenMatrix[random] = maxIndex - 1;
+        } else {
+            // ...otherwise copy over the stored number to the current matrix position.
+            tokenMatrix[random] = tokenMatrix[maxIndex - 1];
+        }
+
+        // Increment counts
+        _tokenCount.increment();
+
+        return value + startFrom;
+    }
+    
+    function linearMint(uint _tries) private onlyOwner {
+        require(_tokenCount.current() + 1 <= maxSupply, 'Cannot mint more than total supply.');
+        
+        for (uint i = 0; i < _tries; i++) {
+            _tokenCount.increment();
+            _safeMint(msg.sender, _tokenCount.current());
+        }
+    }
+    
+    // modifier
+    
+    /// @dev Check whether another token is still available
+    modifier ensureAvailability() {
+        require(availableTokenCount() > 0, "No more tokens available");
+        _;
+    }
+    
+    /// @param amount Check whether number of tokens are still available
+    /// @dev Check whether tokens are still available
+    modifier ensureAvailabilityFor(uint amount) {
+        require(availableTokenCount() >= amount, "Requested number of tokens not available");
+        _;
     }
 }
